25696a0256a64df8f03e4e47c003f8308ab5b3e0
Kombath-S <155440031+Kombath-S@users.noreply.github.com>
2024-01-14 00:31:50 +0000

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/sui-mai-restaurant/",
+  base: "/sui-mai-restaurant",
   server: {
     // Configuration pour la gestion des fichiers statiques
     fs: {
